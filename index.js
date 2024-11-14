2e4cdb6f1c022f0a88e3fb31551e22b9476b1834
IgnacioCortes <ignacio.cortes.g@mail.pucv.cl>
2024-11-14 18:42:53 -0300

    3.5

--- index.js
+++ index.js
@@ -71,6 +71,31 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+// POST notes/?
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(p => p.id))
+      : 0
+    return maxId + 1
+  }
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+    const person = {
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    }
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
 
 // Servidor
 const PORT = 3001
